34fba4f79b179b3d1b389b85e11440ce0490a367
anci-flower <2289342907@qq.com>
2019-07-21 00:50:03 +0800

    Updates

---
+++ precache-manifest.56bfa0407e6afa3d377bf5d18aaca843.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f291a4eeeab992eeaf13d06ca70114",
+    "url": "/swipecard/index.html"
+  },
+  {
+    "revision": "e38ee649b8c021d6a89a",
+    "url": "/swipecard/static/css/main.87f2ff25.chunk.css"
+  },
+  {
+    "revision": "daa31f6663532aabb2ee",
+    "url": "/swipecard/static/js/2.d190dc04.chunk.js"
+  },
+  {
+    "revision": "e38ee649b8c021d6a89a",
+    "url": "/swipecard/static/js/main.1d5a9552.chunk.js"
+  },
+  {
+    "revision": "88bd6dd1210f9868ace0",
+    "url": "/swipecard/static/js/runtime~main.2dd38853.js"
+  }
+]);
